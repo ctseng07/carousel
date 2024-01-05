cd5122759222204753cb9eac6ee37d45561cb8cc
Caleb Tseng <calebtseng07@gmail.com>
2024-01-05 05:03:24 -0800

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import Carousel from './components/carousel';
+
+import { about1, about2, about3, about4, about5 } from '../src/assets/images';
 
 function App() {
+  const slides = [
+    about1,
+    about2,
+    about3,
+    about4,
+    about5
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-[30%] h-full m-auto py-20'>
+      <Carousel slides={slides} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/assets/images/index.js
@@ -0,0 +1,13 @@
+import about1 from './about1.png';
+import about2 from './about2.png';
+import about3 from './about3.png';
+import about4 from './about4.png';
+import about5 from './about5.png';
+
+export {
+    about1,
+    about2,
+    about3,
+    about4,
+    about5
+}
